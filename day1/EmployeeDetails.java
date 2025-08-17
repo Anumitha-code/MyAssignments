2fc7e1ab8f93f33cb0f87e4cd485d45c707480cb
Anumitha-code <anumitharv@gmail.com>
2025-08-17 12:58:30 +0530

    Add files via upload
    day1 assignment

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="John Doe";
+		int age=30;
+		float salary= 55000.75f;
+		char gender='M';
+		boolean acceptTerms=true;
+		float rating=4.5f;
+		long mobileNumber=7896785678L;
+		int pincode=56001;
+		byte luckyNumber=7;
+		
+		System.out.println("Name:"+name+"\n"+"Age:"+age+"\n"+"Salary:"+salary+"\n"
+				+"Gender:"+gender+"\n"+"Accept Terms="+acceptTerms+"\n"+"Rating:"+rating+"\n"+ "Mobile:"+mobileNumber+"\n"+"Pincode:"+pincode+"\n"+"LuckyNumber:"+luckyNumber);
+
+	}
+
+}
